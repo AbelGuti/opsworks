1264647a54673378d01f6b2de03058b2289f548a
Shan <shanmugakarna89@gmail.com>
2018-02-20 11:53:35 +0530

    test opsworks

--- elasticsearch/recipes/configure.rb
+++ elasticsearch/recipes/configure.rb
@@ -1,4 +1,2 @@
 this_instance   = search("aws_opsworks_instance", "self:true").first
-Chef::Log.error(this_instance)
-Chef::Log.error(this_instance["layer_ids"][0])
-Chef::Log.error(search("aws_opsworks_layer", "layer_ids:#{this_instance["layer_ids"]}"))
+Chef::Log.error(search("aws_opsworks_layer", "layer_ids:#{this_instance["layer_ids"][0]}"))
