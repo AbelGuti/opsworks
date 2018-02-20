d5a760d403ea4a8cab1d2160189ebf584036a5af
Shan <shanmugakarna89@gmail.com>
2018-02-20 11:32:04 +0530

    test opsworks

---
+++ elasticsearch/recipes/configure.rb
@@ -0,0 +1,4 @@
+this_instance   = search("aws_opsworks_instance", "self:true").first
+search("aws_opsworks_instance", "layer_id:#{this_instance["layer_ids"][0]}").each do |instance|
+  Chef::Log.error(instance)
+end
