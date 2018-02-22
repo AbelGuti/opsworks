6bac2f660b8eb81e69bfe76d179139bf808513b7
Shan <shanmugakarna89@gmail.com>
2018-02-22 05:31:37 +0530

    add plugins for 2.x

--- elasticsearch/recipes/setup.rb
+++ elasticsearch/recipes/setup.rb
@@ -20,3 +20,15 @@ end
 package 'elasticsearch' do
   action :install
 end
+if node['elasticsearch']['version'] == "2.x" then
+  execute 'install_elasticsearch_head' do
+    command '/usr/share/elasticsearch/bin/plugin install mobz/elasticsearch-head'
+    action :run
+    creates "/usr/share/elasticsearch/plugins/head"
+  end
+  execute 'install_elasticsearch_head' do
+    command '/usr/share/elasticsearch/bin/plugin install lmenezes/elasticsearch-kopf/v2.1.1'
+    action :run
+    creates "/usr/share/elasticsearch/plugins/kopf"
+  end
+end
