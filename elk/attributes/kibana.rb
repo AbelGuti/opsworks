b8a54ce9920b7ec21214038795eeef572b81ce3a
Shan <shanmugakarna89@gmail.com>
2018-04-11 05:02:27 +0530

    elk stack

---
+++ elk/attributes/kibana.rb
@@ -0,0 +1,4 @@
+
+default['kibana']['checksums']['6.2.3']['debian'] = 'b54a1b685656a1424d4956e48daed923752fc268b79bb1b8616cc91f6a78e3bb'
+default['kibana']['checksums']['6.2.3']['rhel'] = 'd513a6f82436914c35e774529686e5fdfed1af77264e39228e5d64eee22c78ce'
+default['kibana']['checksums']['6.2.3']['tarball'] = '01dd8dec5f0acf04336721e404bf4d075675a3acae9f2a9fdcdbb5ca11baca76'
