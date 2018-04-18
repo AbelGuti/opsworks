2cff2d98ef9ce4143fef127d2e3d9c3c756681a4
Shanmuga Sundaram K <shanmugakarna>
2018-04-18 22:39:09 +0530

    jenkins

--- jenkins/metadata.rb
+++ jenkins/metadata.rb
@@ -18,12 +18,10 @@ chef_version '>= 12.14' if respond_to?(:chef_version)
 # a Supermarket.
 #
 # source_url 'https://github.com/<insert_org_here>/jenkins'
-depends 'jenkins', '= 5.0.6'
 depends 'java', '~> 1.50.0'
 depends 'apt', '= 5.1.0'
 depends 'homebrew', '= 3.0.0'
 depends 'windows', '= 2.1.1'
-depends 'dpkg_autostart', '~> 0.2.0'
 depends 'compat_resource', '~> 12.19.0'
 depends 'ohai', '= 4.2.3'
 depends 'git', '= 5.0.2'
@@ -32,4 +30,3 @@ depends 'build-essential', '= 7.0.3'
 depends 'dmg', '= 3.1.1'
 depends 'yum-epel', '~> 2.1.1'
 depends 'limits', '~> 1.0.0'
-depends 'packagecloud', '= 0.3.0'
