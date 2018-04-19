5e096a81bd24966fff0498c755fa29a808a2e2fe
Abel Gutierrez <vegetalink333@gmail.com>
2018-04-19 13:08:20 -0600

    elasticsearch 6.x

--- elasticsearch/recipes/configure.rb
+++ elasticsearch/recipes/configure.rb
@@ -6,18 +6,37 @@ search("aws_opsworks_instance", "layer_ids:#{layer_id}").each do |instance|
 end
 master_nodes_quorum = (search("aws_opsworks_instance", "layer_ids:#{layer_id}").size / 2) + 1
 
-template '/etc/elasticsearch/elasticsearch.yml' do
-  source 'elasticsearch.yml.erb'
-  owner 'root'
-  group 'elasticsearch'
-  mode '0744'
-  variables({
-    elasticsearch_nodes: elasticsearch_nodes,
-    master_nodes_quorum: master_nodes_quorum,
-    node_name: 	this_instance["hostname"],
-    rack_id: this_instance["availability_zone"]
-  })
-  notifies :restart, 'service[elasticsearch]', :delayed
+if node['elasticsearch']['version'] == "6.x" then
+  template '/etc/elasticsearch/elasticsearch.yml' do
+    source 'elasticsearch.yml.erb'
+    owner 'root'
+    group 'elasticsearch'
+    mode '0744'
+    variables({
+      elasticsearch_nodes: elasticsearch_nodes,
+      master_nodes_quorum: master_nodes_quorum,
+      node_name: 	this_instance["hostname"],
+      node_rack: "node.attr.rack_id"
+      rack_id: this_instance["availability_zone"]
+    })
+    notifies :restart, 'service[elasticsearch]', :delayed
+  end
+else
+  template '/etc/elasticsearch/elasticsearch.yml' do
+    source 'elasticsearch.yml.erb'
+    owner 'root'
+    group 'elasticsearch'
+    mode '0744'
+    variables({
+      elasticsearch_nodes: elasticsearch_nodes,
+      master_nodes_quorum: master_nodes_quorum,
+      node_name: 	this_instance["hostname"],
+      node_rack: "node.rack_id"
+      rack_id: this_instance["availability_zone"]
+      xpack: "xpack.license.self_generated.type: basic"
+    })
+    notifies :restart, 'service[elasticsearch]', :delayed
+  end
 end
 
 service 'elasticsearch' do
